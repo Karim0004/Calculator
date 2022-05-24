a237c8b4f53746ba8f7e719a5418fb2b421f144f
Karim <karimx099@gmail.com>
2022-05-24 19:25:55 +0200

    add functions that evaluate an array of numbers and operators

--- script.js
+++ script.js
@@ -41,11 +41,43 @@ function subtract (n1, n2) {
 }
 
 function operate (operator, n1, n2) {
-    if (operator === 'x') return (mulitply(n1, n2));
+    if (operator === 'x') return (multiply(n1, n2));
     else if (operator === '÷') return (divide(n1, n2));
     else if (operator === '+') return (add(n1, n2));
     else if (operator === '-') return (subtract(n1, n2));
 }
+
+
+const operatorsOrder = [['x', '÷'], ['+', '-']]
+
+// operate the given operators in an array
+function operateArray(operators, array) {
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === operators[0] || array[i] === operators[1]) {
+            if (isNaN(array[i+1])) return false;
+            array.splice(i-1, 3, operate(array[i], array[i-1], array[i+1]));
+        }
+    }
+    return true;
+}
+
+
+
+function calculate (array) {
+    
+    // Error checking //
+    if (isNaN(array[array.length-1])) return 'ERROR: Cannot end with an operator';
+    if (isNaN(array[0])) return 'ERROR: Cannot start with an operator';
+
+    /* operate on array, first do add and subtract, then multiplication and division.
+    function operateArray returns false if an operator is not followed by a number */
+    if (!operateArray(operatorsOrder[0], array)) return 'ERROR: A number must follow an operator';
+    if (!operateArray(operatorsOrder[1], array)) return 'ERROR: A number must follow an operator';
+    return (array[0]);
+}
+
+
 
 
 
